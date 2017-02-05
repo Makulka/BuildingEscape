e9631b9fafa60ee98bda26418ce7bfd64bd455c6
Stana <s.svobodova83@gmail.com>
2017-02-05 14:42:21 +0000

    The addition of class PositionReport, reporting the position of objects in the output log when project is run

---
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "PositionReport.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	//To get names of objects with this class
+	FString ObjectName = GetOwner()->GetName();
+
+	//To get position of objects with this class
+	FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
+
+	//To dipsplay the object position in the outputlog
+	UE_LOG(LogTemp, Warning, TEXT("%s at position %s"), *ObjectName, *ObjectPosition);
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
